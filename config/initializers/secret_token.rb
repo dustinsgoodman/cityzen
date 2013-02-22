dda10dfd3efd00d2ce60ef930239ced7ab953b3f
Dustin Goodman <dustin.goodman@clickscape.com>
2013-02-22 18:42:45 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamPlebGovathon::Application.config.secret_token = '187d076b2c04151822b1195f09bda9e15101a95519c4c704230a7a9f868cd17c8f440dbd1976536ea3199891f786231dc39b457fe0553901a12d409e0928d9fa'
