8d037e2e1dfacfe22cd4d63431e8402dbc74078d
Dustin Goodman <dustin.goodman@clickscape.com>
2013-02-22 22:01:51 -0500

    schema fix from merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223022223) do
+ActiveRecord::Schema.define(:version => 20130223022611) do
 
   create_table "petitions", :force => true do |t|
     t.string  "title"
@@ -22,5 +22,49 @@ ActiveRecord::Schema.define(:version => 20130223022223) do
     t.float   "latitude"
     t.text    "content"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name",                                            :null => false
+    t.string   "middle_name"
+    t.string   "last_name",                                             :null => false
+    t.string   "phone_number"
+    t.string   "login",               :limit => 20
+    t.string   "email",               :limit => 100,                    :null => false
+    t.string   "address"
+    t.string   "address2"
+    t.string   "city",                                                  :null => false
+    t.string   "state",                                                 :null => false
+    t.string   "zip",                                                   :null => false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "profile_pic"
+    t.string   "facebook"
+    t.string   "twitter"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token",                                     :null => false
+    t.string   "single_access_token",                                   :null => false
+    t.string   "perishable_token",                                      :null => false
+    t.integer  "login_count",                        :default => 0,     :null => false
+    t.integer  "failed_login_count",                 :default => 0,     :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "password_reset_at"
+    t.datetime "email_confimred_at"
+    t.datetime "tos_agreed_at"
+    t.boolean  "admin",                              :default => false
+    t.boolean  "moderator",                          :default => false
+    t.boolean  "event_coord",                        :default => false
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+  add_index "users", ["oauth_token"], :name => "index_users_on_oauth_token"
 
 end
