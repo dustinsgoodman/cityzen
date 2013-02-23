6de495bdf0073068ba4bc7a72834810076088ec5
Dustin Goodman <dustin.goodman@clickscape.com>
2013-02-23 04:33:59 -0500

    removed redundant line

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,6 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(params[:post])
-    @post.user_id = current_user.id
 
     respond_to do |format|
       if @post.save
