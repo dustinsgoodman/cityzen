2bc37101975944924aaf3a3f75a972fccf627c3d
MRider <bisham3@gatech.edu>
2013-02-23 11:30:06 -0500

    removing redundancy

--- app/models/legislation.rb
+++ app/models/legislation.rb
@@ -1,5 +1,5 @@
 class Legislation < ActiveRecord::Base
-  validates_presence_of :content, :date, :title, :num_for_votes, :num_no_votes, :date
+  validates_presence_of :content, :date, :title, :num_for_votes, :num_no_votes
   attr_accessible :affected_location, :content, :date, :legislation_type, :num_for_votes, :num_no_votes, :ordinance_id, :status, :sponsor, :title
   after_save :set_status
 
