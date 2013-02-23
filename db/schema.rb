dd2ddcb993d43e6efd219458f416d7b4f8e55181
Dustin Goodman <dustin.goodman@clickscape.com>
2013-02-23 12:07:22 -0500

    new schema post migrate

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(:version => 20130223165047) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "consumer_tokens", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "type",       :limit => 30
+    t.string   "token",      :limit => 1024
+    t.string   "secret"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+  end
+
+  add_index "consumer_tokens", ["token"], :name => "index_consumer_tokens_on_token", :unique => true, :length => {"token"=>100}
+
   create_table "legislations", :force => true do |t|
     t.string   "title"
     t.text     "content"
